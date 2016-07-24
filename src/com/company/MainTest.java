ff407dea51e62c4f87148e6ac7268203769055a4
Na Yang <na.yang@hotmail.com>
2016-07-23 22:45:31 -0400

    Add test method.

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 public class MainTest {
     @Test
-    public void add() throws Exception {
+    public void add1Plus1() throws Exception {
         int sum = Main.add(1, 2);
         assertEquals(3, sum);
+    }
+
+    @Test
+    public void add0Plus1() throws Exception {
+        int sum = Main.add(0, 1);
+        assertEquals(1, sum);
     }
 }
